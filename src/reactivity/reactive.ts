232a6351eb2eb59686b5831f5a4966bf24aba1c1
pilipala233 <449186451@qq.com>
2024-03-22 09:40:04 +0800

    feat(reactive): 实现readonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -16,3 +16,16 @@ export function reactive(raw){
         }
     })
 } 
+export function readonly(raw){
+    return new Proxy(raw, {
+        get(target, key){
+                const res = Reflect.get(target, key)
+         
+                return res
+        },
+        set(target, key, value){
+
+            return true
+        }
+    })
+} 
