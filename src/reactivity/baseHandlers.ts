91beedc916e5a03af6c9ee047c81b6632c3a3e73
pilipala233 <449186451@qq.com>
2024-03-25 10:05:32 +0800

    feat(reactive): 响应式对象的嵌套对象处理
    reactive&readonly

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,5 +1,6 @@
 import { track, trigger } from "./effect"
-import { ReactiveFlags } from "./reactive"
+import { ReactiveFlags, reactive, readonly } from "./reactive"
+import { isObject } from "./shared"
 
 const get = createGetter()
 
@@ -17,6 +18,11 @@ function createGetter(isReadonly = false){
 
 
         const res = Reflect.get(target, key)
+
+
+        if(isObject(res)){
+            return isReadonly ? readonly(res) : reactive(res)
+        }
         if(!isReadonly){
             //依赖收集
             track(target, key)
