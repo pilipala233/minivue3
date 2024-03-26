509e3d216b691152a34b8c59015d9f34a9231356
pilipala233 <449186451@qq.com>
2024-03-26 10:56:04 +0800

    refactor(ref): 优化 ref 代码实现

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -1,104 +1,71 @@
-// import { trackEffects, triggerEffects, isTracking } from "./effect";
-// import { createDep } from "./dep";
-// import { isObject, hasChanged } from "@mini-vue/shared";
+
 import { isTracking, trackEffects, triggerEffects } from "./effect";
 import { reactive } from "./reactive";
+import { hasChanged, isObject } from "./shared";
 
 export class RefImpl {
-  private _rawValue: any;
+
   private _value: any;
   public dep;
-  public __v_isRef = true;
-
+  private _rawValue
   constructor(value) {
-    this._value = value;
-    // 看看value 是不是一个对象，如果是一个对象的话
-    // 那么需要用 reactive 包裹一下
-    //this._value = convert(value);
+    this._rawValue = value;
+    this._value = convert(value) ;
+
     this.dep = new Set();
   }
 
   get value() {
-    // 收集依赖
-    if (isTracking()){
-      trackEffects(this.dep);
-    }
-    
-   // trackRefValue(this);
+    // 收集依赖 
+   trackRefValue(this);
     return this._value;
   }
 
   set value(newValue) {
-    // 当新的值不等于老的值的话，
+    // 当新的值不等于老的值的话，还要考虑初始化时已经变成了代理对象，而set时是普通对象，所以不能直接hasChanged(newValue, this._value)了
     // 那么才需要触发依赖
-    if(Object.is(newValue, this._value))return
+    if(hasChanged(newValue, this._rawValue)){
       // 更新值
-      this._value = newValue;
+
+      this._rawValue = newValue;
+      this._value = convert(newValue);
       
       // 触发依赖
-      triggerEffects(this.dep);
+      triggerEffects(this.dep); 
+    }
+
     
   }
 }
+
+
+function convert(value) {
+  //优化
+  return isObject(value) ? reactive(value) : value;
+}
+
+export function trackRefValue(ref) {
+  if (isTracking()) {
+    trackEffects(ref.dep);
+  }
+}
+
 
 export function ref(value) {
   return new RefImpl(value);
 }
 
-// function convert(value) {
-//   return isObject(value) ? reactive(value) : value;
-// }
-
-// function createRef(value) {
-//   const refImpl = new RefImpl(value);
-
-//   return refImpl;
-// }
-
-// export function triggerRefValue(ref) {
-//   triggerEffects(ref.dep);
-// }
-
-// export function trackRefValue(ref) {
-//   if (isTracking()) {
-//     trackEffects(ref.dep);
-//   }
-// }
-
-// 这个函数的目的是
-// 帮助解构 ref
-// 比如在 template 中使用 ref 的时候，直接使用就可以了
-// 例如： const count = ref(0) -> 在 template 中使用的话 可以直接 count
-// 解决方案就是通过 proxy 来对 ref 做处理
-
-const shallowUnwrapHandlers = {
-  get(target, key, receiver) {
-    // 如果里面是一个 ref 类型的话，那么就返回 .value
-    // 如果不是的话，那么直接返回value 就可以了
-    return unRef(Reflect.get(target, key, receiver));
-  },
-  set(target, key, value, receiver) {
-    const oldValue = target[key];
-    if (isRef(oldValue) && !isRef(value)) {
-      return (target[key].value = value);
-    } else {
-      return Reflect.set(target, key, value, receiver);
-    }
-  },
-};
-
-// 这里没有处理 objectWithRefs 是 reactive 类型的时候
-// TODO reactive 里面如果有 ref 类型的 key 的话， 那么也是不需要调用 ref.value 的
-// （but 这个逻辑在 reactive 里面没有实现）
 export function proxyRefs(objectWithRefs) {
-  return new Proxy(objectWithRefs, shallowUnwrapHandlers);
+  
 }
 
 // 把 ref 里面的值拿到
 export function unRef(ref) {
-  return isRef(ref) ? ref.value : ref;
+  
 }
 
 export function isRef(value) {
-  return !!value.__v_isRef;
+  
 }
+
+
